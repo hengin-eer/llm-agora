a2bb78992642f8093f849d63de7beb7524672964
hengin-eer <hirokix0608@gmail.com>
2025-07-07 03:20:24 +0900

    ✨ #3 システムプロンプトをターンごとに変更する

--- src/app/debate/Textarea.tsx
+++ src/app/debate/Textarea.tsx
@@ -1,16 +1,46 @@
 "use client";
 
+import { getSystemPrompt } from "@/lib/llm/prompt-pool";
+import type { ChatRequestOptions } from "ai";
+import { type ChangeEvent, type FormEvent, useState } from "react";
+
 type TextAreaProps = {
-  input: string;
-  handleSubmit: () => void;
-  handleInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+	input: string;
+	handleSubmit: (
+		event?: {
+			preventDefault?: () => void;
+		},
+		chatRequestOptions?: ChatRequestOptions,
+	) => void;
+	handleInputChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
 function TextArea({ input, handleSubmit, handleInputChange }: TextAreaProps) {
+	const [turnNumber, setTurnNumber] = useState(0);
+	const systemPrompt = getSystemPrompt(turnNumber);
+
+	function incrementTurn() {
+		setTurnNumber((prev) => prev + 1);
+	}
+	console.log("turnNumber", turnNumber);
+	console.log("systemPrompt", systemPrompt);
+
+	function handleOnSubmit(e: FormEvent<HTMLFormElement>) {
+		incrementTurn();
+		handleSubmit(e, {
+			body: {
+				nextSystem: systemPrompt,
+			},
+		});
+	}
+
 	return (
-		<form onSubmit={handleSubmit} className="box-border flex items-end gap-2 w-full">
+		<form
+			onSubmit={(e) => handleOnSubmit(e)}
+			className="box-border flex items-end gap-2 w-full"
+		>
 			<div className="flex-1 w-full">
-        {/* TODO: UIレイアウト崩れ＆改行時の自動拡張が無効化してしまったので別Issueでfixしよう */}
+				{/* TODO: UIレイアウト崩れ＆改行時の自動拡張が無効化してしまったので別Issueでfixしよう */}
 				<textarea
 					className="px-4 py-2 h-full w-full resize-none ring rounded-md"
 					placeholder="メッセージを入力..."
